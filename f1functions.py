a4a844d7be724bbb133aa2a5914b018cef78f5ee
rads03 <121972297+rads03@users.noreply.github.com>
2024-09-11 21:03:56 +0530

    Update f1functions.py

--- f1functions.py
+++ f1functions.py
@@ -232,8 +232,8 @@ def compare_driver_stats(df, driver_1, driver_2, stat, year, driver_mappings):
     telemetry2 = df.loc[int(driver2)]
     
     fig, ax = plt.subplots(figsize=(14, 6))
-    sns.lineplot(x=telemetry1.index, y=telemetry1[stat], label=driver_1, linewidth=3.5, color='cornflowerblue')
-    sns.lineplot(x=telemetry2.index, y=telemetry2[stat], label=driver_2, linewidth=3.5, color='orange')
+    sns.lineplot(x=telemetry1.Time, y=telemetry1[stat], label=driver_1, linewidth=3.5, color='cornflowerblue')
+    sns.lineplot(x=telemetry2.Time, y=telemetry2[stat], label=driver_2, linewidth=3.5, color='orange')
     
     ax.set_xlabel('')
     ax.set_ylabel('')
